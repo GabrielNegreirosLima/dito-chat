83bca38e9a738ffd9eab28a3a2ad3b4c647e0dda
Gabriel Negreiros <gabrielnegreiroslima@gmail.com>
2019-03-01 15:33:12 -0300

    Inclui /build

---
+++ frontend/build/precache-manifest.fe74523c8abfed86fab92abcdf36d7d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bfa079d398d8c6073b9e",
+    "url": "/static/js/main.bfa079d3.chunk.js"
+  },
+  {
+    "revision": "eea9697113b36c42dccf",
+    "url": "/static/js/2.eea96971.chunk.js"
+  },
+  {
+    "revision": "bfa079d398d8c6073b9e",
+    "url": "/static/css/main.25d56cab.chunk.css"
+  },
+  {
+    "revision": "eea9697113b36c42dccf",
+    "url": "/static/css/2.0f32312a.chunk.css"
+  },
+  {
+    "revision": "f393e025e7266871cd60cde7d65e17b2",
+    "url": "/index.html"
+  }
+];
